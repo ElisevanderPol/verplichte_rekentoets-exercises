ae4ef9d77255d756d81b7e74085c08f3ae1c507c
Elise van der Pol <koster.elise@gmail.com>
2014-01-13 16:44:53 +0100

    London eye exercise

--- utils/math-format.js
+++ utils/math-format.js
@@ -598,5 +598,43 @@ $.extend(KhanUtil, {
         var exponent = KhanUtil.scientificExponent(num);
         var mantissa = KhanUtil.localeToFixed(KhanUtil.scientificMantissa(precision, num), precision);
         return "" + mantissa + "\\times 10^{" + exponent + "}";
+    },
+    
+    floatToTime: function(num){
+      var stringed = num.toString();
+      strArr = stringed.split('.');
+      console.log(strArr);
+      
+      if(strArr[1] == 25){
+        //var mins == ':15';
+        return strArr[0] + ':15';
+      }
+      else if(strArr[1] == 5){
+        //var mins == ':30';
+        return strArr[0] + ':30';
+      }
+      else if(strArr[1] == 45){
+        //var mins == ':45';
+        return strArr[0] + ':45';
+      }
+      else{
+       // var mins == ':00';
+       return strArr[0] + ':00';
+      }
+
+    },
+    
+    lengthToRides: function(length, ride){
+      var rideLen = 0;
+      if (ride == 15){
+        rideLen == 25;
+      }
+      else if(ride == 30){
+        rideLen == 50;
+      }
+      else{
+        rideLen == 75
+      }
     }
+    
 });
